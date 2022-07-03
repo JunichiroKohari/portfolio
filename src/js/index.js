32f1998abb13f4bcaf6d6f912af27f089d5a8fb2
JunichiroKohari <junichiro@kohari.jp>
2022-07-03 21:35:07 +0900

    import variable script

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-alert('hoge')
+import $ from 'jquery'
+// import 'modaal'
+import createREGL from 'regl'
+import './shader.js'
+import AOS from 'aos'
+import './script.js'
+
+import '../style/normalize.css'
+import '../style/style.css'
